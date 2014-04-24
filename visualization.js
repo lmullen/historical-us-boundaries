17a348380ee8dff75766d6d0e3371ebea86b9f5e
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-04-23 23:35:51 -0400

    Display the map at any given date

--- visualization.js
+++ visualization.js
@@ -11,6 +11,8 @@ var svg = d3.select("#viz").append("svg")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var dispDate = new Date("1849-01-01");
+
 function ready(error, us) { 
   console.log(us);
 
@@ -23,8 +25,19 @@ function ready(error, us) {
   var path = d3.geo.path()
     .projection(projection);
   
-  svg.append("path")
-    .datum(states)
-    .attr("d", path);
+  svg
+    .selectAll(".states")
+    .data(states.features)
+    .enter()
+    .append("path")
+    .attr("class", function(d) { 
+      return "unit " + d.id + " " + d.properties.TERR_TYPE; 
+    })
+    .attr("d", path)
+    .filter(function(d) {
+      return Date.parse(d.properties.START_DATE) < dispDate &&
+             dispDate < Date.parse(d.properties.END_DATE);
+    })
+    .classed("active", true);
 }  
 
