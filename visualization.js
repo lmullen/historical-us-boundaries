abe76230e9d84192ae4b3469907ce6ea55ffad81
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-04-24 20:14:02 -0400

    Tooltips

--- visualization.js
+++ visualization.js
@@ -11,10 +11,16 @@ var svg = d3.select("#viz").append("svg")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var dispDate = new Date("1849-01-01");
+var dispDate = new Date(1861,6,04);
+
+d3.select("#date").html("<strong>" + niceDate(dispDate) + "</strong>");
 
 function ready(error, us) { 
   console.log(us);
+  console.log(us.objects.states.geometries[0]); 
+
+  var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip");
 
   var states = topojson.feature(us, us.objects.states);
 
@@ -30,14 +36,40 @@ function ready(error, us) {
     .data(states.features)
     .enter()
     .append("path")
+    .attr("title", function(d,i) { return d.properties.TERR_TYPE; })
     .attr("class", function(d) { 
       return "unit " + d.id + " " + d.properties.TERR_TYPE; 
     })
     .attr("d", path)
     .filter(function(d) {
-      return Date.parse(d.properties.START_DATE) < dispDate &&
-             dispDate < Date.parse(d.properties.END_DATE);
+      console.log(new Date(d.properties.START_DATE).toString());
+      return Date.parse(d.properties.START_DATE) <= dispDate &&
+             dispDate <= Date.parse(d.properties.END_DATE);
     })
-    .classed("active", true);
+    .classed("active", true)
+    .on("mousemove", function(d, i) {
+      var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+      tooltip
+        .classed("hidden", false)
+        .attr("style", "left:" + (mouse[0]+30)+"px; top:" + mouse[1] + "px")
+        .html("<h4>" + d.properties.FULL_NAME + "</h4><p>" +
+              "<strong>Type:</strong> " + d.properties.TERR_TYPE + "<br>" +
+              "<strong>Boundary begin:</strong> " + niceDate(d.properties.START_DATE) + "<br>" +
+              "<strong>Boundary end:</strong> " + niceDate(d.properties.END_DATE) + "<br>" +
+              "<strong>Explanation of boundary change:</strong> " + d.properties.CHANGE + "</p>"
+             ); 
+    })
+    .on("mouseout", function(d, i) {
+      tooltip.classed("hidden", true);
+    });
+
 }  
+
+function niceDate(date) {
+  var dateOptions = {year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    timeZone: "UTC"};
+  return new Date(date).toLocaleDateString("en-US", dateOptions);
+}
 
