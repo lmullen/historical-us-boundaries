2e4cf3dc5d755683fb027a0f93a8aaab2d776413
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-04-29 23:10:16 -0400

    Add ratification to map colors and tooltip

--- visualization.js
+++ visualization.js
@@ -1,24 +1,24 @@
 queue()
-  .defer(d3.json, "us.json")
-  .defer(d3.json, "coast.json")
-  .await(ready);
+.defer(d3.json, "us.json")
+.defer(d3.json, "coast.json")
+.await(ready);
 
 var margin = {top: 10, right: 20, bottom: 10, left: 20};
 var width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+height = 500 - margin.top - margin.bottom;
 var svg = d3.select("#viz").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 slider_height = 20;
 
 var slider_svg = d3.select("#slider").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", slider_height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+.attr("width", width + margin.left + margin.right)
+.attr("height", slider_height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var dispDate = new Date(1783,8,3);
 
@@ -27,199 +27,222 @@ var dispDate = new Date(1783,8,3);
 function ready(error, us, coast) { 
 
   var tooltip = d3.select("#viz").append("div")
-    .classed("hidden", true)
-    .attr("class", "tooltip");
+  .classed("hidden", true)
+  .attr("class", "tooltip");
 
   var states = topojson.feature(us, us.objects.states);
   var coastline  = topojson.feature(coast, coast.objects.coast);
 
   // var projection = d3.geo.mercator()
   var projection = d3.geo.albers()
-    .scale(1100)
-    .translate([ 0.5 * width, 0.5 * height]);
+  .scale(1100)
+  .translate([ 0.5 * width, 0.5 * height]);
 
   var path = d3.geo.path()
-    .projection(projection);
-  
-  svg
-    .selectAll(".unit")
-    .data(states.features)
-    .enter()
-    .append("path")
-    .attr("class", function(d) { 
-      return "unit " + d.id + " " + d.properties.TERR_TYPE; 
-    })
-    .attr("d", path);
-    // .filter(function(d) {
-    //   return Date.parse(d.properties.START_DATE) <= dispDate &&
-    //          dispDate <= Date.parse(d.properties.END_DATE);
-    // })
-    // .classed("active", true)
-    // .on("mousemove", function(d, i) {
-    //   var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-    //   tooltip
-    //     .classed("hidden", false)
-    //     .attr("style", "left:" + (mouse[0]+30)+"px; top:" + mouse[1] + "px")
-    //     .html("<h4>" + d.properties.FULL_NAME + "</h4><p>" +
-    //           "<strong>Type:</strong> " + d.properties.TERR_TYPE + "<br>" +
-    //           "<strong>Boundary begin:</strong> " + niceDate(d.properties.START_DATE) + "<br>" +
-    //           "<strong>Boundary end:</strong> " + niceDate(d.properties.END_DATE) + "<br>" +
-    //           "<strong>Explanation of boundary change:</strong> " + d.properties.CHANGE + "</p>"
-    //          ); 
-    // })
-    // .on("mouseout", function(d, i) {
-    //   tooltip.classed("hidden", true);
-    // });
+  .projection(projection);
 
   svg
-    .selectAll(".coastline")
-    .data(coastline.features)
-    .enter()
-    .append("path")
-    .attr("class", "coast")
-    .attr("d", path);
+  .selectAll(".unit")
+  .data(states.features)
+  .enter()
+  .append("path")
+  .attr("class", function(d) { 
+    return "unit " + d.id + " " + d.properties.TERR_TYPE; 
+  })
+  .attr("d", path);
+  // .filter(function(d) {
+  //   return Date.parse(d.properties.START_DATE) <= dispDate &&
+  //          dispDate <= Date.parse(d.properties.END_DATE);
+  // })
+  // .classed("active", true)
+  // .on("mousemove", function(d, i) {
+  //   var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+  //   tooltip
+  //     .classed("hidden", false)
+  //     .attr("style", "left:" + (mouse[0]+30)+"px; top:" + mouse[1] + "px")
+  //     .html("<h4>" + d.properties.FULL_NAME + "</h4><p>" +
+  //           "<strong>Type:</strong> " + d.properties.TERR_TYPE + "<br>" +
+  //           "<strong>Boundary begin:</strong> " + niceDate(d.properties.START_DATE) + "<br>" +
+  //           "<strong>Boundary end:</strong> " + niceDate(d.properties.END_DATE) + "<br>" +
+  //           "<strong>Explanation of boundary change:</strong> " + d.properties.CHANGE + "</p>"
+  //          ); 
+  // })
+  // .on("mouseout", function(d, i) {
+  //   tooltip.classed("hidden", true);
+  // });
+
+  svg
+  .selectAll(".coastline")
+  .data(coastline.features)
+  .enter()
+  .append("path")
+  .attr("class", "coast")
+  .attr("d", path);
 
   // Slider
   var start = new Date(1783, 8, 3),
-      end   = new Date(1912, 11, 31);
+  end   = new Date(1912, 11, 31);
 
   var x = d3.time.scale()
-      .domain([start, end])
-      .range([0, width])
-      .clamp(true);
+  .domain([start, end])
+  .range([0, width])
+  .clamp(true);
 
   var brush = d3.svg.brush()
-      .x(x)
-      .extent([0, 0])
-      .on("brush", brushed);
+  .x(x)
+  .extent([0, 0])
+  .on("brush", brushed);
 
   slider_svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + slider_height / 2 + ")")
-      .call(d3.svg.axis()
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + slider_height / 2 + ")")
+  .call(d3.svg.axis()
         .scale(x)
         .orient("bottom")
         .tickFormat(function(d) { return d.getFullYear(); })
         .tickSize(0)
         .tickPadding(12))
-    .select(".domain")
-    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-      .attr("class", "halo");
+        .select(".domain")
+        .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+        .attr("class", "halo");
 
-  var slider = slider_svg.append("g")
-      .attr("class", "slider")
-      .call(brush);
+        var slider = slider_svg.append("g")
+        .attr("class", "slider")
+        .call(brush);
 
-  slider.selectAll(".extent,.resize")
-      .remove();
+        slider.selectAll(".extent,.resize")
+        .remove();
 
-  slider.select(".background")
-      .attr("height", slider_height);
+        slider.select(".background")
+        .attr("height", slider_height);
 
-  var handle = slider.append("circle")
-      .attr("class", "handle")
-      .attr("transform", "translate(0," + slider_height / 2 + ")")
-      .attr("r", 9);
+        var handle = slider.append("circle")
+        .attr("class", "handle")
+        .attr("transform", "translate(0," + slider_height / 2 + ")")
+        .attr("r", 9);
 
-  slider
-      .call(brush.event)
-      .call(brush.extent([dispDate, dispDate]))
-      .call(brush.event);
+        slider
+        .call(brush.event)
+        .call(brush.extent([dispDate, dispDate]))
+        .call(brush.event);
 
-  tooltip.classed("hidden", true);
+        tooltip.classed("hidden", true);
 
-  var date_label = svg
-    .append("g")
-    .append("text")
-    .classed("date-label", true)
-    .classed("map-label", true)
-    .attr("text-anchor", "start")
-    .attr("x", width - 125)
-    .attr("y", 220)
-    .text(niceDate(dispDate));
+        var date_label = svg
+        .append("g")
+        .append("text")
+        .classed("date-label", true)
+        .classed("map-label", true)
+        .attr("text-anchor", "start")
+        .attr("x", width - 125)
+        .attr("y", 210)
+        .text(niceDate(dispDate));
 
-  var legend = svg
-    .append("g");
+        var legend = svg
+        .append("g");
 
-  labels = ["State", "Unorganized", "Territory",
-            "Seceded", "Reconstruction", "Other"];
+        labels = ["Unratified", "State", "Unorganized", "Territory",
+          "Seceded", "Reconstruction", "Other"];
 
-  for(i = 0; i < labels.length; i++) {
+          for(i = 0; i < labels.length; i++) {
 
-    legend
-      .append("circle")
-      .classed("unit", true)
-      .classed("active", true)
-      .classed(labels[i], true)
-      .attr("cx", width - 115)
-      .attr("cy", 250 + i * 30)
-      .attr("r", 10);
+            legend
+            .append("circle")
+            .classed("unit", true)
+            .classed("active", true)
+            .classed(labels[i], true)
+            .attr("cx", width - 115)
+            .attr("cy", 240 + i * 30)
+            .attr("r", 10);
 
-    legend
-      .append("text")
-      .classed("map-label", true)
-      .attr("x", width - 100)
-      .attr("y", 256 + i * 30)
-      .attr("text-anchor", "start")
-      .text(labels[i]);
+            legend
+            .append("text")
+            .classed("map-label", true)
+            .attr("x", width - 100)
+            .attr("y", 246 + i * 30)
+            .attr("text-anchor", "start")
+            .text(labels[i]);
 
-  }
+          }
 
-  function brushed() {
-    var value = brush.extent()[0];
+          function brushed() {
+            var value = brush.extent()[0];
 
-    if (d3.event.sourceEvent) { // not a programmatic event
-      value = x.invert(d3.mouse(this)[0]);
-      brush.extent([value, value]);
-    }
+            if (d3.event.sourceEvent) { // not a programmatic event
+              value = x.invert(d3.mouse(this)[0]);
+              brush.extent([value, value]);
+            }
 
-    handle.attr("cx", x(value));
-    
-    svg.selectAll(".date-label").text(niceDate(value));
+            handle.attr("cx", x(value));
 
-    svg
-    .selectAll(".unit")
-    .data(states.features)
-    .classed("active", false)
-    .filter(function(d) {
-      return Date.parse(d.properties.START_DATE) <= value &&
-             value <= Date.parse(d.properties.END_DATE);
-    })
-    .classed("active", true)
-    .classed("Seceded", function(d) {
-      return Date.parse(d.properties.secession_start) <= value &&
-             value <= Date.parse(d.properties.reconstruction_start);
-    })
-    .classed("Reconstruction", function(d) {
-      return Date.parse(d.properties.reconstruction_start) <= value &&
-             value < Date.parse(d.properties.reconstruction_end);
-    })
-    .on("mousemove", function(d, i) {
-      var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-      var offset = d3.select("#viz")[0][0].offsetLeft;
+            svg.selectAll(".date-label").text(niceDate(value));
 
-      tooltip
-        .classed("hidden", false)
-        .attr("style", "left:" + (mouse[0] + offset + 40)+"px; top:" + mouse[1] + "px")
-        .html("<h4>" + d.properties.FULL_NAME + "</h4><p>" +
-              "<strong>Type:</strong> " + d.properties.TERR_TYPE + "<br>" +
-              "<strong>Boundary begin:</strong> " + niceDate(d.properties.START_DATE) + "<br>" +
-              "<strong>Boundary end:</strong> " + niceDate(d.properties.END_DATE) + "<br>" +
-              "<strong>Explanation of boundary change:</strong> " + d.properties.CHANGE + "</p>"
-             ); 
-    })
-    .on("mouseout", function(d, i) {
-      tooltip.classed("hidden", true);
-    });
+            svg
+            .selectAll(".unit")
+            .data(states.features)
+            .classed("active", false)
+            .filter(function(d) {
+              return Date.parse(d.properties.START_DATE) <= value &&
+                value <= Date.parse(d.properties.END_DATE);
+            })
+            .classed("active", true)
+            .classed("Seceded", function(d) {
+              return Date.parse(d.properties.secession_start) <= value &&
+                value <= Date.parse(d.properties.reconstruction_start);
+            })
+            .classed("Reconstruction", function(d) {
+              return Date.parse(d.properties.reconstruction_start) <= value &&
+                value < Date.parse(d.properties.reconstruction_end);
+            })
+            .classed("Unratified", function(d) {
+              return value < Date.parse(d.properties.ratified);
+            })
+            .on("mousemove", function(d, i) {
+              var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+              var offset = d3.select("#viz")[0][0].offsetLeft;
 
-  }
+              tooltip
+              .classed("hidden", false)
+              .attr("style", "left:" + (mouse[0] + offset + 40)+"px; top:" + mouse[1] + "px")
+              .html("<h4>" + d.properties.FULL_NAME + "</h4><p>" +
+                  ratified(d) + 
+                  seceded(d) +
+                  reconstruction(d) +
+                  "<strong>Boundary begin:</strong> " + niceDate(d.properties.START_DATE) + "<br>" +
+                  "<strong>Boundary end:</strong> " + niceDate(d.properties.END_DATE) + "<br>" +
+                  "<strong>Explanation of boundary change:</strong> " + d.properties.CHANGE + "</p>"
+                   ); 
+            })
+            .on("mouseout", function(d, i) {
+              tooltip.classed("hidden", true);
+            });
+
+          }
 }  
 
 function niceDate(date) {
   var dateOptions = {year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                    timeZone: "UTC"};
-  return new Date(date).toLocaleDateString("en-US", dateOptions);
+    month: "long",
+    day: "numeric",
+    timeZone: "UTC"};
+    return new Date(date).toLocaleDateString("en-US", dateOptions);
+}
+
+function ratified(d) {
+  if (d.properties.ratified) {
+  return "<strong>Ratified Constitution:</strong> " + d.properties.ratified + "<br>";
+  } else {return "";}
+}
+
+function seceded(d) {
+  if (d.properties.secession_start) {
+  return "<strong>Seceded:</strong> " + d.properties.secession_start + "<br>";
+  } else {return "";}
+}
+
+function reconstruction(d) {
+  if (d.properties.reconstruction_end) {
+  return "<strong>Re-admitted to Congress:</strong> " + d.properties.reconstruction_end + "<br>";
+  } else {return "";}
 }
 
